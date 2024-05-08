ebcebb66467f1aef943710189b6898f3d6da65e2
ArseneBrosy <arsene.brosy@divtec.ch>
2024-05-08 11:26:40 +0200

    stick

--- js/script.js
+++ js/script.js
@@ -5,26 +5,77 @@ let ctx = canvas.getContext("2d");
 canvas.width = 2000;
 canvas.height = 2000 * canvas.clientHeight / canvas.clientWidth;
 
-//region CONSTANTES
+//region CONSTANTS
 //endregion
 
 //region VARIABLES
-// souris
-let mouseX = 0;
-let mouseY = 0;
+// mouse
+let mouse = {
+    x: 0,
+    y: 0
+};
+
+// player
+let player = {
+    x: canvas.width / 2,
+    y: 0,
+    stickAngle: 0,
+
+    WIDTH: 100,
+    HEIGHT: 100
+};
+
+let camera = {
+    y: -canvas.height + player.HEIGHT / 2
+}
 //endregion
 
 //region FUNCTIONS
 //endregion
 
 setInterval(() => {
+    //region PHYSICS
+    //endregion
+
     //region DRAW
     // clear
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.height = 2000 * canvas.clientHeight / canvas.clientWidth;
+    camera.y = -canvas.height + player.HEIGHT / 2;
+
+    // player
+    ctx.fillStyle = "red";
+    ctx.fillRect(player.x - player.WIDTH / 2, player.y - player.HEIGHT / 2 - camera.y, player.WIDTH, player.HEIGHT);
+
+    // stick
+    ctx.fillStyle = "blue";
+    ctx.translate(player.x, player.y - camera.y);
+    ctx.rotate(player.stickAngle * (Math.PI/180));
+    ctx.fillRect(-10, -50, 20, 100);
+    ctx.rotate(-player.stickAngle * (Math.PI/180));
+    ctx.translate(-player.x, -player.y - camera.y);
+
     //endregion
 }, 0);
 
 document.addEventListener("mousemove", (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
+    mouse.x = e.clientX / canvas.clientWidth * canvas.width;
+    mouse.y = e.clientY / canvas.clientHeight * canvas.height;
+
+    let dirX = mouse.x - player.x;
+    let dirY = mouse.y - (player.y - camera.y);
+
+    let angle = Math.atan(dirY/dirX)*(180/Math.PI);
+    angle += 90;
+    if (dirX < 0) {
+        angle -= 180;
+    }
+    player.stickAngle = angle;
+});
+
+document.addEventListener("mousedown", (e) => {
+    if (e.button === 0) {
+        let forceY = Math.cos(player.stickAngle * (Math.PI/180));
+        let forceX = Math.sin(player.stickAngle * (Math.PI/180));
+        console.log(forceX + " : " + forceY);
+    }
 });
